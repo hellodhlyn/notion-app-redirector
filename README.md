b969221bb74c8f42fe5c60e3774f47c715c0d659
Do Hoerin <hellodhlyn@gmail.com>
2018-06-05 22:31:22 +0900

    Hello, world!

---
+++ README.md
@@ -0,0 +1,7 @@
+# notion-app-redirector
+
+> Open [notion](https://www.notion.so) documents with your desktop application directly.
+
+## Tested Browsers
+
+  - Firefox
---
+++ background.js
@@ -0,0 +1,29 @@
+// Paths listed below shouldn't be redirect to application.
+const exceptingPaths = [
+  '/desktop', '/login', '/mobile', '/product', '/pricing',
+]
+
+function redirect(details) {
+  // Ignore urls which are not for document pages, such as js/css file and more.
+  if (details.documentUrl) {
+    return {};
+  }
+  
+  const element = document.createElement('a');
+  element.href = details.url;
+
+  // Ignore some reserved keywords for notion homepage.
+  if (!element.pathname 
+      || element.pathname === '/'
+      || exceptingPaths.find(path => element.pathname.startsWith(path))
+  ) {
+    return {};
+  }
+
+  // Redirect with scheme.
+  const notionScheme = 'notion:/' + element.pathname;
+  return { redirectUrl: notionScheme };
+}
+
+const filters = { urls: ['*://*.notion.so/*'] };
+browser.webRequest.onBeforeRequest.addListener(redirect, filters, ['blocking']);
