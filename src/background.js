3e8c1d3017385100fcc2e506ec9d0bdf4826d245
Bouke van der Bijl <i@bou.ke>
2023-06-14 11:59:17 +0200

    Add all remaining exceptions

--- src/background.js
+++ src/background.js
@@ -1,6 +1,121 @@
 // Paths listed below shouldn't be redirect to application.
+// Taken from https://www.notion.so/apple-app-site-association
 const exceptingPaths = [
-  '/desktop', '/login', '/mobile', '/product', '/pricing', '/native', '/invoice', '/web-clipper', '/help', '/templates', '/releases'
+  "/@",
+  "/about",
+  "/affiliates",
+  "/ai",
+  "/android",
+  "/api-beta",
+  "/appleauthcallback",
+  "/applepopupcallback",
+  "/applepopupredirect",
+  "/asanaauthcallback",
+  "/asanapopupredirect",
+  "/blog",
+  "/careers",
+  "/community",
+  "/compare-against",
+  "/components",
+  "/confluence",
+  "/consultants",
+  "/contact-sales",
+  "/customers",
+  "/da-dk",
+  "/de-de",
+  "/desktop",
+  "/docs",
+  "/educators",
+  "/email-confirm",
+  "/en-us",
+  "/enterprise",
+  "/es-es",
+  "/es-la",
+  "/events",
+  "/evernote",
+  "/evernoteoauthcallback",
+  "/evernotepopupredirect",
+  "/externalauthcallback",
+  "/externalintegrationauthcallback",
+  "/externalintegrationpopupredirect",
+  "/fi-fi",
+  "/fr-fr",
+  "/front-api",
+  "/front-static",
+  "/googledrivepickerpopup",
+  "/googledrivepopupredirect",
+  "/googlepopupcallback",
+  "/googlepopupredirect",
+  "/guide",
+  "/guides",
+  "/help",
+  "/help",
+  "/install-integration",
+  "/integrations",
+  "/invoice",
+  "/ja-jp",
+  "/jobs",
+  "/join-us",
+  "/ko-kr",
+  "/ko",
+  "/login",
+  "/loginpasswordreset",
+  "/loginwithemail",
+  "/logout",
+  "/mobile",
+  "/my-integrations",
+  "/native/oauth2callback",
+  "/nb-no",
+  "/nl-nl",
+  "/nonprofits",
+  "/notes",
+  "/oauth2callback",
+  "/onboarding",
+  "/pages",
+  "/passwordchangeredirect",
+  "/passwordresetcallback",
+  "/personal",
+  "/pricing",
+  "/product",
+  "/product/ai",
+  "/product/docs",
+  "/product/projects",
+  "/product/wikis",
+  "/projects",
+  "/pseudo",
+  "/pt-br",
+  "/releases",
+  "/remote",
+  "/resources",
+  "/samlauthcallback",
+  "/sandbox",
+  "/security",
+  "/signup",
+  "/sitemap.xml",
+  "/slackoauthcallback",
+  "/slackpopupredirect",
+  "/sso/saml",
+  "/startups",
+  "/storybook",
+  "/students",
+  "/sv-se",
+  "/teams",
+  "/template-preview",
+  "/templates",
+  "/tools-and-craft",
+  "/trelloauthcallback",
+  "/trellopopupredirect",
+  "/unsubscribe",
+  "/upgraded-account",
+  "/use-case",
+  "/web-clipper",
+  "/webinars",
+  "/why",
+  "/wiki",
+  "/wikis",
+  "/work",
+  "/zh-cn",
+  "/zh-tw",
 ];
 
 async function redirect(details) {
@@ -9,13 +124,14 @@ async function redirect(details) {
     return {};
   }
 
-  const element = document.createElement('a');
+  const element = document.createElement("a");
   element.href = details.url;
 
   // Ignore some reserved keywords for notion homepage.
-  if (!element.pathname
-      || element.pathname === '/'
-      || exceptingPaths.find(path => element.pathname.startsWith(path))
+  if (
+    !element.pathname ||
+    element.pathname === "/" ||
+    exceptingPaths.find((path) => element.pathname.startsWith(path))
   ) {
     return {};
   }
@@ -24,9 +140,13 @@ async function redirect(details) {
   const settings = await loadSettings();
   const keepTabOpen = settings[settingKeepTabOpenKey] || false;
 
-  const notionScheme = details.url.replace('https', 'notion');
+  const notionScheme = details.url.replace("https", "notion");
   if (keepTabOpen) {
-    browser.tabs.create({ url: notionScheme }).then((tab) => setTimeout(() => { browser.tabs.remove(tab.id) }, 5000));
+    browser.tabs.create({ url: notionScheme }).then((tab) =>
+      setTimeout(() => {
+        browser.tabs.remove(tab.id);
+      }, 5000)
+    );
     return {};
   } else {
     try {
@@ -39,5 +159,5 @@ async function redirect(details) {
   }
 }
 
-const filters = { urls: ['https://*.notion.so/*'] };
-browser.webRequest.onBeforeRequest.addListener(redirect, filters, ['blocking']);
+const filters = { urls: ["https://*.notion.so/*"] };
+browser.webRequest.onBeforeRequest.addListener(redirect, filters, ["blocking"]);
